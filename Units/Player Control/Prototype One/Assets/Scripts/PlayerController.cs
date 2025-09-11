a2c895f436608600160be3a3478830974a052041
SFO24072001 <SFO24072001@sfcollege.edu>
2025-09-11 19:07:12 -0400

    09112025

--- Units/Player Control/Prototype One/Assets/Scripts/PlayerController.cs
+++ Units/Player Control/Prototype One/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,66 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+//[RequireComponent (typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
-    [SerializeField] float turnSpeed = 45.0f;
-    private float horizontalInput, forwardInput;
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    
+
+    private Vector2 InputMovement;
+
+    public InputActionReference move;
+
+    private Rigidbody rb;
+    Collider left_wheel;
+
+    enum InputDirections
+    {
+        Horizontal=99,
+        Vertical
+    }
+    enum GrowthCycle
+    {
+        barren,
+        grow1,
+        grow2,
+        havrest,
+        seedling,
+        sprout,        
+    }
+    private GrowthCycle growthCycle;
+    [SerializeField] private Sprite[] growthImages;
+    private Sprite currentImages;
+
+    enum boolean
+    {
+        False,
+        True,
+    }
+
+    private void Awake()
+    {
+        if (growthCycle == GrowthCycle.grow1)
+        {
+            currentImages = growthImages[(int)growthCycle];
+        }
+
+        rb = GetComponent<Rigidbody>();
+        left_wheel = GetComponentsInChildren<Collider>().SingleOrDefault(q => q.name == "Wheel_fl");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //InputMovement = new Vector2(Input.GetAxis(InputDirections.Horizontal.ToString()), Input.GetAxis(InputDirections.Vertical.ToString()));
+
+        InputMovement = move.action.ReadValue<Vector2>();
 
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * InputMovement.y);
         // Turns the vehicle
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * InputMovement.x * Time.deltaTime);
     }
 }
